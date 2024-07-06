1310950afd1aabadcdf3358048c54c427f2f2c57
mickaelgrima <mickaelgrima974@gmail.com>
2024-07-06 14:19:10 +0400

    add command arguments to execution

--- services/collector/src/main.py
+++ services/collector/src/main.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import argparse
 
 from scrapers import (
     discover_timekeepers as discover_timekeepers_scrapers,
@@ -11,6 +12,10 @@ from database import client as db_client, Database
 from controller import BackgroundController
 
 logger = logging.getLogger(__name__)
+
+scrap_all_type = 'all'
+scrap_timekeepers_type = 'timekeepers'
+scrap_metadata_type = 'metadata'
 
 
 async def run_single_results_scraper(scraper: ResultsScraper, db: Database):
@@ -52,23 +57,41 @@ async def run_metadata_scraper(scraper: MetadataScraper, db: Database):
         await asyncio.sleep(0.01)
 
 
-async def run():
+async def run(type_: str = scrap_all_type, scrapers: list[str] = None):
     # set-up logging
     logging.basicConfig()
     logging.getLogger().setLevel(logging.INFO)
 
     async with db_client() as db:
-        # first fetch the data from timekeepers
-        await asyncio.gather(*[
-            run_single_results_scraper(scraper, db)
-            for scraper in discover_timekeepers_scrapers()
-        ])
-        # then fetch metadata (elevations for example)
-        await asyncio.gather(*[
-            run_metadata_scraper(scraper, db)
-            for scraper in discover_metadata_scrapers()
-        ])
+        if type_ in {scrap_all_type, scrap_timekeepers_type}:
+            # first fetch the data from timekeepers
+            await asyncio.gather(*[
+                run_single_results_scraper(scraper, db)
+                for scraper in discover_timekeepers_scrapers(scrapers=scrapers)
+            ])
+        if type_ in {scrap_all_type, scrap_metadata_type}:
+            # then fetch metadata (elevations for example)
+            await asyncio.gather(*[
+                run_metadata_scraper(scraper, db)
+                for scraper in discover_metadata_scrapers(scrapers=scrapers)
+            ])
 
 
 if __name__ == '__main__':
-    asyncio.run(run())
+    # parse args
+    parser = argparse.ArgumentParser(description='Define what scraper(s) to run')
+    parser.add_argument(
+        '-t', '--type',
+        choices=[scrap_all_type, scrap_metadata_type, scrap_timekeepers_type],
+        default=scrap_all_type,
+        help='What kind of scrapers do we run: timekeepers, metadata, or both?'
+    )
+    parser.add_argument(
+        '-s', '--scrapers',
+        default='',
+        help='comma-separated list of scrapers to run.'
+    )
+    args = parser.parse_args()
+
+    # run
+    asyncio.run(run(type_=args.type, scrapers=args.scrapers or None))
--- services/collector/tests/scrapers/test_main.py
+++ services/collector/tests/scrapers/test_main.py
@@ -1,15 +1,53 @@
+import pytest
+
 from ..context import scrapers
+from scrapers import MetadataScraper, ResultsScraper
 from scrapers.sportpro import SportproScraper
 from scrapers.runraid import RunRaidScraper
 
 
-def test_discover_metadata_scrapers():
-    res = scrapers.discover_metadata_scrapers()
-    assert len(res) == 1
-    assert isinstance(res[0], RunRaidScraper)
+def is_equal(
+        res: list[MetadataScraper | ResultsScraper],
+        expected: list[MetadataScraper | ResultsScraper]
+):
+    assert len(res) == len(expected)
+    for i in range(len(res)):
+        assert isinstance(res[i], expected[i].__class__)
 
 
-def test_discover_timekeepers():
-    res = scrapers.discover_timekeepers()
-    assert len(res) == 1
-    assert isinstance(res[0], SportproScraper)
+@pytest.mark.parametrize(
+    "scrapers_,expected",
+    [
+        (None, [RunRaidScraper()]),
+        (['runraid'], [RunRaidScraper()]),
+    ]
+)
+def test_discover_metadata_scrapers(
+        scrapers_: list[str] | None,
+        expected: list[MetadataScraper] | Exception):
+    res = list(scrapers.discover_metadata_scrapers(scrapers=scrapers_))
+    is_equal(res, expected)
+
+
+def test_discover_metadata_scrapers_error():
+    with pytest.raises(KeyError):
+        scrapers.discover_metadata_scrapers(scrapers=["unknown"])
+
+
+@pytest.mark.parametrize(
+    "scrapers_,expected",
+    [
+        (None, [SportproScraper()]),
+        (['sportpro'], [SportproScraper()]),
+    ]
+)
+def test_discover_timekeepers(
+        scrapers_: list[str] | None,
+        expected: list[ResultsScraper] | Exception):
+    res = list(scrapers.discover_timekeepers(scrapers=scrapers_))
+    is_equal(res, expected)
+
+
+def test_discover_timekeepers_scrapers_error():
+    with pytest.raises(KeyError):
+        scrapers.discover_timekeepers(scrapers=["unknown"])
